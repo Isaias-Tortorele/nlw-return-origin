5194382bf8f26fae7ce5a6b520a785e04bc3b55f
Isaias-Tortorele <isaiastortorele23@gmail.com>
2022-05-21 20:52:42 -0300

    Projeto final

--- main.js
+++ main.js
@@ -4,6 +4,47 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonScroll()
+  activeMenuAtCurrentSection(home)
+  activeMenuAtCurrentSection(services)
+  activeMenuAtCurrentSection(about)
+  activeMenuAtCurrentSection(contact)
+}
+
+function activeMenuAtCurrentSection(section) {
+  // linha alvo
+  const targeLine = scrollY + innerHeight / 2
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo?
+  const sectionTopReachOrPassedTargetline = targeLine >= sectionTop
+  console.log(
+    'o Topo da seção chegou ou passou da linha alvo?',
+    sectionTopReachOrPassedTargetline
+  )
+
+  //a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targeLine
+  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetline)
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectioId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectioId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
